527af585500e11a1e2251292cb2f129a15b9079c
Choony93 <ngchoonheng@outlook.com>
2017-05-17 16:11:34 +0800

    upload test crawler

---
+++ DatabaseHelper/crawler/URLCrawler.py
@@ -0,0 +1,30 @@
+'''
+Created on May 14, 2017
+
+@author: Choony
+'''
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'http://www.sp.edu.sg/wps/portal/vp-spws/spws.fsu.cse',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+        
+
+process = CrawlerProcess()
+process.crawl(QuotesSpider)
+process.start() # the script will block here until the crawling is finished
